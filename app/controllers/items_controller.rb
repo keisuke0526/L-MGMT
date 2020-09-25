9e07f5e0c548db2ac7db0ccfbd800cd2c0b052b5
keisuke.f <f.keisuke0526@gmail.com>
2020-09-25 22:57:42 +0900

    initial commit

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,40 @@
+class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if  @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
+  end
+
+  private
+    def item_params
+      params.require(:item).permit(:guest_name, :pickup_day, :houseroom, :room_no, :phone_number)
+    end
+
+end
---
+++ db/migrate/20200925134648_create_items.rb
@@ -0,0 +1,13 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.string :guest_name, null: false
+      t.date :pickup_day, null: false
+      t.integer :houseroom, null: false
+      t.integer :room_no
+      t.string :phone_number
+      t.string  :remark
+      t.timestamps
+    end
+  end
+end
