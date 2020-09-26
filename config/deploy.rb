915cf1b8692fccae84a7a05de32bc608d7d51162
keisuke.f <f.keisuke0526@gmail.com>
2020-09-26 23:30:44 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,18 @@
+# config valid only for current version of Capistrano
+# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
 lock '3.14.1'
 
 # Capistranoのログの表示に利用する
 set :application, 'L-MGMT'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:kaisuke0526/L-MGMT.git'
+set :repo_url,  'git@github.com:keisuke0526/L-MGMT.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.5.3'
+set :rbenv_ruby, '2.5.3' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
